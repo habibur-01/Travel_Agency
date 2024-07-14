4ffe3bbe6a37ce6cb7e20026e3a8f7cc65fbdb38
habibur-01 <h.r.habibur08@gmail.com>
2024-07-14 06:09:04 +0600

    update script file

--- script/script.js
+++ script/script.js
@@ -8,20 +8,19 @@ function toggleNav() {
   } else {
     navbar.classList.remove("show-nav");
   }
-  console.log("Navbar is now " + (showNav ? "visible" : "hidden"));
 }
 
 
-const initSlider = () => {
-    const testimonals = document.querySelector(".slider-wrapper .testimonial-cards")
-    const slideButtons = document.querySelectorAll(".slider-wrapper .slider-btn")
-    slideButtons.forEach(element => {
-        element.addEventListener("click", () => {
-           const direction = element.id === "prev-slide" ? -1 : 1;
-           const scrollAmount = testimonals.clientWidth * direction;
-           testimonals.scrollBy({left:scrollAmount, behavior: "smooth"})
-        })
+// const initSlider = () => {
+//     const testimonals = document.querySelector(".slider-wrapper .testimonial-cards")
+//     const slideButtons = document.querySelectorAll(".slider-wrapper .slider-btn")
+//     slideButtons.forEach(element => {
+//         element.addEventListener("click", () => {
+//            const direction = element.id === "prev-slide" ? -1 : 1;
+//            const scrollAmount = testimonals.clientWidth * direction;
+//            testimonals.scrollBy({left:scrollAmount, behavior: "smooth"})
+//         })
         
-    });
-}
-window.addEventListener("load", initSlider)
+//     });
+// }
+// window.addEventListener("load", initSlider)
